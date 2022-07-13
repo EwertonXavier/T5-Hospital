2d3a24403324296bd957ed1f7b1082178666bd4b
Neddlf43 <ewerton.freitas@live.com>
2022-07-13 09:49:11 -0400

    Add project files.

---
+++ T5-Hospital/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(T5_Hospital.Startup))]
+namespace T5_Hospital
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
